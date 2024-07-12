c96174fcb3deaf757187c77369f131daa84319df
jyt6640 <jyt6640@gmail.com>
2024-07-12 09:36:36 +0900

    첫상태 수정

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\AIPROJECT\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\Google_Cloud_Ai_Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
